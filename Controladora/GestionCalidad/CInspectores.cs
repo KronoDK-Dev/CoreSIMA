7c87ef874031a1a9afc0a6652fff1c1ba82392c7
Mnq140503 <1277626@senati.pe>
2025-11-18 15:29:52 -0500

    #Fusion - GestionCalidad

---
+++ Controladora/GestionCalidad/CInspectores.cs
@@ -0,0 +1,25 @@
+﻿using EntidadNegocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos.NoTransaccional.GestionCalidad;
+using AccesoDatos.Transaccional.GestionCalidad;
+
+namespace Controladora.GestionCalidad
+{
+    public class CInspectores
+    {
+        public string ModificaInserta(BaseBE oBaseBE)
+        {
+            return (new InspectoresTAD()).ModificaInserta(oBaseBE);
+        }
+
+        public DataTable ListarTodos(string Id1, string UserName)
+        {
+            return (new InspectoresNTAD()).ListarTodos(Id1, UserName);
+        }
+    }
+}
---
+++ Controladora/GestionCalidad/CProyectoActividad.cs
@@ -0,0 +1,30 @@
+﻿using EntidadNegocio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos.NoTransaccional.GestionCalidad;
+using AccesoDatos.Transaccional.GestionCalidad;
+
+namespace Controladora.GestionCalidad
+{
+    public class CProyectoActividad
+    {
+        public DataTable ListarTodos(string Id1, string UserName)
+        {
+            return (new ProyectoActividadNTAD()).ListarTodos(Id1, UserName);
+        }
+
+        public DataTable Buscar(string TextFind, string UserName)
+        {
+            return (new ProyectoActividadNTAD()).Buscar(TextFind, UserName);
+        }
+
+        public string ModificaInserta(BaseBE oBaseBE)
+        {
+            return (new ProyectoActividadTAD()).ModificaInserta(oBaseBE);
+        }
+    }
+}

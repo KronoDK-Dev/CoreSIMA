8641c3992fea97ce040402ba6bb0697599d6662d
vybanez <vybanez@simaperu.com>
2025-12-16 09:22:52 -0500

    conflicto 3

---
+++ Controladora/GestionFinanciera/cReporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos.NoTransaccional.GestionFinanciera;
+
+namespace Controladora.GestionFinanciera
+{
+    public class cReporte
+    {
+        public DataTable GenRptAnexosFinanciero(int IdFormato, int Periodo, string RangoMes, int IdUsuario,
+            string UserName)
+        {
+            return (new Reportes()).GenRptAnexosFinanciero(IdFormato, Periodo, RangoMes, IdUsuario,
+                UserName);
+        }
+
+        public DataTable GenRptAnexosFiltro(string IdProceso, string UserName)
+        {
+            return (new Reportes()).GenRptAnexosFiltro(IdProceso, UserName);
+        }
+    }
+}

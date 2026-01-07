5ce6d3ccb0ec42100cbe5d25e6ab45888b55bd8b
erosalesPRY <ejra2008@gmail.com>
2026-01-07 14:34:59 -0500

    Add files via upload

--- Controladora/HelpDesk/ITIL/CProcedimientoNota.cs
+++ Controladora/HelpDesk/ITIL/CProcedimientoNota.cs
@@ -1,32 +1,31 @@
-﻿using EntidadNegocio;
+﻿using AccesoDatos.NoTransaccional.HelpDesk.ITIL;
+using AccesoDatos.Transaccional.HelpDesk.ITIL;
+using EntidadNegocio;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AccesoDatos.NoTransaccional.HelpDesk.ITIL;
-using AccesoDatos.Transaccional.HelpDesk.ITIL;
 
 namespace Controladora.HelpDesk.ITIL
 {
-    public class CProcedimientoNota
+    public  class CProcedimientoNota
     {
         public DataTable ListarTodos(string Id1, string Id2, string UserName)
         {
             return (new ProcedimientoNotaNTAD()).ListarTodos(Id1, Id2, UserName);
         }
-
         public BaseBE Detalle(string Id1, string Id2, string UserName)
         {
             return (new ProcedimientoNotaNTAD()).Detalle(Id1, Id2, UserName);
         }
 
+
         public string ModificaInserta(BaseBE oBaseBE)
         {
-            return (new ProcedimientoNotaTAD()).ModificaInserta(oBaseBE);
+            return(new ProcedimientoNotaTAD()).ModificaInserta(oBaseBE);
         }
-
         public int Eliminar(string Id1, string Id2, string Id3)
         {
             return (new ProcedimientoNotaTAD()).Eliminar(Id1, Id2, Id3);

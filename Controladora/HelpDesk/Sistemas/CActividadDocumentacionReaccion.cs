ae4c1582b3887b9f634ff9507b929ce143a619f9
erosalesPRY <ejra2008@gmail.com>
2026-01-07 14:35:42 -0500

    Add files via upload

--- Controladora/HelpDesk/Sistemas/CActividadDocumentacionReaccion.cs
+++ Controladora/HelpDesk/Sistemas/CActividadDocumentacionReaccion.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using AccesoDatos.NoTransaccional.HelpDesk.Sistemas;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AccesoDatos.NoTransaccional.HelpDesk.Sistemas;
 
 namespace Controladora.HelpDesk.Sistemas
 {
-    public class CActividadDocumentacionReaccion
+    public  class CActividadDocumentacionReaccion
     {
         public DataTable ListarTodos(string Id1, string UserName)
-        {
-            return (new ActividadDocumentacionReaccionNTAD()).ListarTodos(Id1, UserName);
+        { 
+            return (new ActividadDocumentacionReaccionNTAD()).ListarTodos(Id1, UserName);   
         }
     }
 }

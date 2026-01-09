67bc1c43d9c4a4cccbd4f930fb3d27f0cf33cf03
erosalesPRY <ejra2008@gmail.com>
2026-01-09 08:33:29 -0500

    Add files via upload

--- AccesoDatos/Transaccional/GestionFinanciera/Tesoreria/ComprobanteRetencionTAD.cs
+++ AccesoDatos/Transaccional/GestionFinanciera/Tesoreria/ComprobanteRetencionTAD.cs
@@ -4,24 +4,22 @@ using Oracle.DataAccess.Client;
 using System;
 using System.Data;
 using Utilitario;
+
 namespace AccesoDatos.Transaccional.GestionFinanciera.Tesoreria
 {
-    public class ComprobanteTAD:BaseAD
+    public class ComprobanteRetencionTAD:BaseAD
     {
-
-
 
         public int ActualizarEstado(string TipoDoc, string NroSer, int Estado, int CentroOperativo)
         {
-            string UserName="";
+            string UserName = "";
             int IdProceso = 0;
             try
             {
                 string PackagName = "";
                 if (CentroOperativo == Convert.ToInt32(Enumerados.CentroOperativo.SimaCallao))
-                {
-                    PackagName = "O7INVOICE.Pd_Comprobante_Venta_Pkg.ComprobanteEst_Tra";
-
+                { 
+                    PackagName = "Pd_Comprobante_Venta_Pkg.ComprobanteEst_Tra";
                     OracleParameter[] Param = new OracleParameter[3];
 
                     Param[0] = new OracleParameter("p_tip_doc", OracleDbType.Varchar2);
@@ -36,21 +34,20 @@ namespace AccesoDatos.Transaccional.GestionFinanciera.Tesoreria
                     Param[2].Direction = ParameterDirection.Input;
                     Param[2].Value = Estado;
 
-                   
                     object id = Oracle(ORACLEVersion.O7).ExecuteScalar(true, PackagName, Param);
                 }
-                else if ((CentroOperativo == Convert.ToInt32(Enumerados.CentroOperativo.SimaChimbote))|| (CentroOperativo == Convert.ToInt32(Enumerados.CentroOperativo.SimaIquitos)))
-                {
+                else if (CentroOperativo == Convert.ToInt32(Enumerados.CentroOperativo.SimaChimbote))
+                { 
                     PackagName = "sp_FECComprobanteEst_Tra";
                     int idResult = Convert.ToInt32(Sql(SQLVersion.sqlDBSimaCH).ExecuteNonQuery(PackagName, TipoDoc, NroSer, Estado));
                 }
-                else if (CentroOperativo == Convert.ToInt32(Enumerados.CentroOperativo.SimaIquitos))
-                {
+                else
+                { 
                     PackagName = "sp_FECComprobanteEst_Tra";
                     int idResult = Convert.ToInt32(Sql(SQLVersion.sqlDBSimaIQ).ExecuteNonQuery(PackagName, TipoDoc, NroSer, Estado));
-
                 }
-                //object OBJ = DBGeneric((Enumerados.CentroOperativo)System.Enum.Parse(typeof(Enumerados.CentroOperativo), CentroOperativo.ToString())).ExecuteNonQuerys(PackagName, Param);
+
+               // object OBJ = DBGeneric((Enumerados.CentroOperativo)System.Enum.Parse(typeof(Enumerados.CentroOperativo), CentroOperativo.ToString())).ExecuteNonQuerys(PackagName, Param);
                 IdProceso = 1;
 
                 return IdProceso;
@@ -58,10 +55,9 @@ namespace AccesoDatos.Transaccional.GestionFinanciera.Tesoreria
             catch (System.Data.SqlClient.SqlException sqlException)
             {
                 LogTransaccional.LanzarSIMAExcepcionDominio(UserName, this.GetType().Name, Utilitario.Enumerados.LogCtrl.OrigenError.AccesoDatos.ToString(), Utilitario.Constante.Archivo.Prefijo.PREFIJOCODIGOERRORNTAD.ToString() + Helper.Cadena.CortarTextoDerecha(5, Utilitario.Constante.LogCtrl.CEROS + sqlException.Number.ToString()), "Código de Error:" + sqlException.Number.ToString() + Utilitario.Constante.Caracteres.SeperadorSimple + "Número de Línea:" + "1" + Utilitario.Constante.Caracteres.SeperadorSimple + sqlException.Message);
-                
                 return IdProceso;
             }
-            catch (Oracle .DataAccess.Client.OracleException oleDbException)
+            catch (System.Data.OleDb.OleDbException oleDbException)
             {
                 LogTransaccional.LanzarSIMAExcepcionDominio(UserName, this.GetType().Name, Utilitario.Enumerados.LogCtrl.OrigenError.AccesoDatos.ToString(), Utilitario.Constante.Archivo.Prefijo.PREFIJOCODIGOERRORNTAD.ToString() + Helper.Cadena.CortarTextoDerecha(5, Utilitario.Constante.LogCtrl.CEROS + oleDbException.ErrorCode.ToString()), "Código de Error:" + oleDbException.ErrorCode.ToString() + Utilitario.Constante.Caracteres.SeperadorSimple + "Número de Línea:" + "1" + Utilitario.Constante.Caracteres.SeperadorSimple + oleDbException.Message);
                 return IdProceso;
@@ -72,7 +68,6 @@ namespace AccesoDatos.Transaccional.GestionFinanciera.Tesoreria
                 return IdProceso;
             }
         }
-      
    
     }
 }
